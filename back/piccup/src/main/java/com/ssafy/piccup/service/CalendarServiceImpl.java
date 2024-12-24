677450468a703fdc74d288e8f607de19a31e820c
Jieun Jung <wldms3333@gmail.com>
2024-12-24 17:27:13 +0900

    Feat: 마이데이터 캘린더 기능 구현중(비로그인)
    마이데이터 캘린더 CRUD 진행

---
+++ back/piccup/src/main/java/com/ssafy/piccup/service/CalendarServiceImpl.java
@@ -0,0 +1,13 @@
+package com.ssafy.piccup.service;
+
+import com.ssafy.piccup.model.dao.CalendarDao;
+
+public class CalendarServiceImpl implements CalendarService {
+	
+	private final CalendarDao calendarDao;
+	
+	public CalendarServiceImpl(CalendarDao calendarDao) {
+		this.calendarDao = calendarDao;
+	}
+
+}
