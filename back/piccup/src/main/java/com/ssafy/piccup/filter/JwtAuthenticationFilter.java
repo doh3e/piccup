8b18f5516ea88667947f6aacefee350dee649026
KSAhh <66674138+KSAhh@users.noreply.github.com>
2024-12-26 19:51:11 +0900

    Comment: 이력서 - 불필요 주석삭제

--- back/piccup/src/main/java/com/ssafy/piccup/filter/JwtAuthenticationFilter.java
+++ back/piccup/src/main/java/com/ssafy/piccup/filter/JwtAuthenticationFilter.java
@@ -28,7 +28,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 			throws ServletException, IOException {
 		
         String token = getJwtFromRequest(request);
-        System.out.println("토큰확인 결과 : "+jwtUtil.checkToken(token));
         if (StringUtils.hasText(token) && jwtUtil.checkToken(token)) {
             int userId = jwtUtil.getUserId(token);
             String email = jwtUtil.getUserEmail(token);
@@ -40,14 +39,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             SecurityContextHolder.getContext().setAuthentication(authentication);
 	        }
 
-        System.out.println("진입");
         filterChain.doFilter(request, response);
     }
 
     private String getJwtFromRequest(HttpServletRequest request) {
         String bearerToken = request.getHeader("Authorization");
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-        	System.out.println("입력: "+bearerToken.substring(7));
             return bearerToken.substring(7);
         }
         return null;
