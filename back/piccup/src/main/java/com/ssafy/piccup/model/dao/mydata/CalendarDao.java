3258ed9de79abe096bf452aea3185e0f933ab9d5
Jieun Jung <wldms3333@gmail.com>
2025-01-01 20:11:48 +0900

    Feat: 자소서 파트 일정 가져오기

--- back/piccup/src/main/java/com/ssafy/piccup/model/dao/mydata/CalendarDao.java
+++ back/piccup/src/main/java/com/ssafy/piccup/model/dao/mydata/CalendarDao.java
@@ -5,12 +5,11 @@ import java.util.List;
 import com.ssafy.piccup.model.dto.mydata.ApplyCalendarSchedule;
 import com.ssafy.piccup.model.dto.mydata.Schedule;
 
-
 public interface CalendarDao {
-	
+
 	// 달력(스케줄) 전체 조회
 	public List<Schedule> selectAllUserSchedule(int userId);
-	
+
 	// 스케줄 추가
 	public void insertSchedule(Schedule schedule);
 
@@ -19,11 +18,10 @@ public interface CalendarDao {
 
 	// 스케줄 수정하기
 	public void updateSchedule(Schedule schedule);
-	
+
 	// 지원 스케줄 전체 조회
 	public List<ApplyCalendarSchedule> selectAllApplySchedules(int userId);
-	
+
 	// 지원 스케줄 상세 조회
 	public ApplyCalendarSchedule selectOneApplyScheduleById(int applyId, int userId);
-	
 }
